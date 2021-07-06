52c0ab7d2f4d31b18571bf969311fccdfad56f3f
ilyaseen19 <alfuty09@gmail.com>
2021-07-06 09:35:34 +0000

    handled tables

--- src/main/app/component/tables.js
+++ src/main/app/component/tables.js
@@ -1,68 +1,121 @@
-import {mdb} from "mdbreact"
+import { MDBDataTableV5 } from "mdbreact";
 
 export default function Tables(props) {
+  const userData = {
+    columns: [
+      {
+        label: "EMPLOYEE NAME",
+        field: "name",
+        width: 250,
+        attributes: {
+          "aria-controls": "DataTable",
+          "aria-label": "Name",
+        },
+      },
+      {
+        label: "PHONE",
+        field: "phone",
+        width: 50,
+      },
+      {
+        label: "USER NAME",
+        field: "userName",
+        width: 50,
+      },
+      {
+        label: "ROLE",
+        field: "role",
+        width: 50,
+      },
+      {
+        label: "Action",
+        field: "action",
+        sort: "disabled",
+        width: 150,
+      },
+    ],
+    rows: props.data.map((user) => {
+      user.name = user.firstName + " " + user.lastName;
+      const action = () => {
+        return (
+          <div
+            type="button"
+            role="button"
+            onClick={() => props.onRowClicked(user)}
+          >
+            <i className="fa fa-ellipsis-v"></i>
+          </div>
+        );
+      };
+      user.action = action();
+      return user;
+    }),
+  };
+  const dishesData = {
+    columns: [
+      {
+        label: "DISH",
+        field: "dishName",
+        width: 250,
+        attributes: {
+          "aria-controls": "DataTable",
+          "aria-label": "Name",
+        },
+      },
+      {
+        label: "PRICE",
+        field: "price",
+        width: 50,
+      },
+      {
+        label: "Action",
+        field: "action",
+        sort: "disabled",
+        width: 150,
+      },
+    ],
+    rows: props.data.map((dish) => {
+      const action = () => {
+        return (
+          <div
+            type="button"
+            role="button"
+            onClick={() => props.onRowClicked(dish)}
+          >
+            <i className="fa fa-ellipsis-v"></i>
+          </div>
+        );
+      };
+      dish.action = action();
+      return dish;
+    }),
+  };
   if (props.title === "users-table") {
     return (
-      <table className="tbody">
-        <thead className="thead">
-          <tr className="thead">
-            <th>EMPLOYEE NAME</th>
-            <th>PHONE</th>
-            <th>USERNAME</th>
-            <th>Role</th>
-            <th>ACTION</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.data.map((user) => {
-            return (
-              <tr>
-                <td>{user.firstName + " " + user.lastName}</td>
-                <td>{user.phone}</td>
-                <td>{user.userName}</td>
-                <td>{user.role}</td>
-                <td
-                  type="button"
-                  role="button"
-                  onClick={() => props.onRowClicked(user)}
-                >
-                  <i className="fa fa-ellipsis-v"></i>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="my-table">
+        <MDBDataTableV5
+          hover
+          entriesOptions={[5, 10, 25]}
+          entries={5}
+          pagesAmount={4}
+          data={userData}
+          searchTop
+          searchBottom={false}
+        />
+      </div>
     );
   } else if (props.title === "dishes-table") {
     return (
-      <div>
-        <table>
-          <thead className="thead">
-            <tr className="thead">
-              <th>DISH</th>
-              <th>PRICE(GHS)</th>
-              <th>ACTION</th>
-            </tr>
-          </thead>
-          <tbody style={{ overflowY: "scroll" }}>
-            {props.data.map((dish) => {
-              return (
-                <tr className="tbody">
-                  <td>{dish.dishName}</td>
-                  <td>{dish.price}</td>
-                  <td
-                    type="button"
-                    role="button"
-                    onClick={() => props.onRowClicked(dish)}
-                  >
-                    <i className="fa fa-ellipsis-v"></i>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div className="my-table">
+        <MDBDataTableV5
+          hover
+          entriesOptions={[5, 10, 25]}
+          entries={5}
+          pagesAmount={4}
+          data={dishesData}
+          searchTop
+          searchBottom={false}
+        />
       </div>
     );
   }
